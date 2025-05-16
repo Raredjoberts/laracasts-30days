6e63b144026209074ad865b1ee08548111af18b7
Jared Roberts <raredjoberts@gmail.com>
2025-05-16 11:58:21 -0500

    Video 16

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,24 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-// Created using `php artisan make:model Job` command
 class Job extends Model {
     use HasFactory;
 
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
 
     public function employer()
     {
         return $this->belongsTo(Employer::class);
     }
-
-    public function tags(){
-        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
-    }
 }
-
