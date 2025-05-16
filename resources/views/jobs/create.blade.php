640bf5fe66b20a11f0345e0ea94fc94d01cb28a9
Jared Roberts <raredjoberts@gmail.com>
2025-05-16 12:41:48 -0500

    Video 18

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -39,15 +39,6 @@
                     </div>
                 </div>
 
-                {{--                <div class="mt-10">--}}
-                {{--                    @if($errors->any())--}}
-                {{--                        <ul>--}}
-                {{--                            @foreach($errors->all() as $error)--}}
-                {{--                                <li class="text-red-500 italic">{{ $error }}</li>--}}
-                {{--                            @endforeach--}}
-                {{--                        </ul>--}}
-                {{--                    @endif--}}
-                {{--                </div>--}}
             </div>
         </div>
 
